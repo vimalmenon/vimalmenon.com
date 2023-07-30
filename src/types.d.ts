71f331714beebf83f71eca633d7414a2e7142a2e
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-07-30 11:04:38 +0800

    [Enhancement] set up types

---
+++ src/types.d.ts
@@ -0,0 +1,8 @@
+import { ReactNode } from 'react';
+
+export interface ReactChildren {
+  children: ReactNode;
+}
+
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
+export type AnyType = any;
