bcab01066806eef96cd1363346e1f8220ae22094
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-07-31 07:24:57 +0800

    Working on Header

---
+++ src/data/index.ts
@@ -0,0 +1,13 @@
+const YouTube = {
+  name: 'YouTube',
+  url: 'https://www.youtube.com/@i_am_vimalmenon',
+};
+const Twitter = {
+  name: 'Twitter',
+  url: 'https://twitter.com/i/flow/login?redirect_after_login=%2FVimalMenon14',
+};
+const Linkedin = {
+  name: 'Linkedin',
+  url: 'https://www.linkedin.com/in/vimal-menon-72ab931a/',
+};
+export const SocialMedias = [YouTube, Twitter, Linkedin];
--- src/layout/PageLayout/Header/Header.component.tsx
+++ src/layout/PageLayout/Header/Header.component.tsx
@@ -1,13 +1,23 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 
+import { SocialMedias } from '@data';
+
 import { HeaderStyled } from './Header.style';
 
 export const Header: React.FC = () => {
   return (
     <HeaderStyled>
       <AppBar component="nav">
-        <Toolbar>Working on Toolbar</Toolbar>
+        <Toolbar>
+          {SocialMedias.map((media) => {
+            return (
+              <a href={media.url} target="_blank" rel="noreferrer" key={media.name}>
+                {media.name}
+              </a>
+            );
+          })}
+        </Toolbar>
       </AppBar>
     </HeaderStyled>
   );
