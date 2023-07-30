f6ff48ba6570e282dc8031ea11e53819734b2510
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-07-30 11:07:17 +0800

    [Chore] Add basic types

--- src/types.d.ts
+++ src/types.d.ts
@@ -6,3 +6,8 @@ export interface ReactChildren {
 
 // eslint-disable-next-line  @typescript-eslint/no-explicit-any
 export type AnyType = any;
+
+export type IGenericMethod = () => void;
+export type IGenericReturn<T> = () => T;
+export type IGenericParam<T> = (v: T) => void;
+export type IGeneric<T, K> = (v: T) => K;
