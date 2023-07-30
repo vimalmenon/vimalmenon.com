8f7cf67096d8743f15d679dd4aff99c891601428
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-07-31 07:59:01 +0800

    Improve :  Header

--- src/layout/PageLayout/Header/Header.component.tsx
+++ src/layout/PageLayout/Header/Header.component.tsx
@@ -1,28 +1,33 @@
 import AppBar from '@mui/material/AppBar';
 import IconButton from '@mui/material/IconButton';
 import Link from '@mui/material/Link';
-import Toolbar from '@mui/material/Toolbar';
+import Tooltip from '@mui/material/Tooltip';
 
 import { SocialMedias } from '@data';
 
-import { HeaderStyled } from './Header.style';
+import { HeaderStyled, HeaderSocialMediaStyled, ToolbarStyled } from './Header.style';
 
 export const Header: React.FC = () => {
   return (
     <HeaderStyled>
       <AppBar component="nav">
-        <Toolbar>
-          {SocialMedias.map((media) => {
-            const { Icon, url, name } = media;
-            return (
-              <Link href={url} target="_blank" rel="noreferrer" key={name}>
-                <IconButton size="small">
-                  <Icon />
-                </IconButton>
-              </Link>
-            );
-          })}
-        </Toolbar>
+        <ToolbarStyled>
+          <div></div>
+          <HeaderSocialMediaStyled>
+            {SocialMedias.map((media) => {
+              const { Icon, url, name } = media;
+              return (
+                <Link href={url} target="_blank" rel="noreferrer" key={name}>
+                  <Tooltip title={name}>
+                    <IconButton size="small">
+                      <Icon />
+                    </IconButton>
+                  </Tooltip>
+                </Link>
+              );
+            })}
+          </HeaderSocialMediaStyled>
+        </ToolbarStyled>
       </AppBar>
     </HeaderStyled>
   );
--- src/layout/PageLayout/Header/Header.style.ts
+++ src/layout/PageLayout/Header/Header.style.ts
@@ -1,5 +1,15 @@
+import Toolbar from '@mui/material/Toolbar';
 import styled from 'styled-components';
 
 export const HeaderStyled = styled('header')(() => ({
-  display: 'grid',
+  display: 'flex',
+}));
+
+export const HeaderSocialMediaStyled = styled('section')(() => ({
+  display: 'flex',
+}));
+
+export const ToolbarStyled = styled(Toolbar)(() => ({
+  display: 'flex',
+  justifyContent: 'space-between',
 }));
